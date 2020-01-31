6a5d2da301f4f5b454df6137dc49ef06ec385050
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-01-31 15:58:40 +0100

    add README and buttons to popup

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-<div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+![Octodo](icons/octopus.png)
+
+# Octodo
+A Firefox extension that creates markdown todos from Github and Gitlab issues.
+
+Tested on FireFox 72.0.2 (64-bit) on Ubuntu 16.04 LTS.
+
+## Development
+Launch a test instance of FireFox with the extension loaded via `web-ext run`.
+
+## Credits
+
+<div><a href="https://www.flaticon.com/free-icon/octopus_1717824">Icon</a> made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
--- browserAction/script.js
+++ browserAction/script.js
@@ -1,15 +1,17 @@
 const executing = browser.tabs.executeScript({file: "/content_script.js"})
 .catch(console.error);
 
-executing.then(createTodo, console.error);
+executing.then(() => {
+  createTodo();
+}, console.error);
 
-async function createTodo() {
+async function createTodo(emoji = '👨‍💻') {
   const {issueTitle, issueNumber, url, error} = await browser.storage.local.get(['issueTitle', 'url', 'issueNumber', 'error']);
 
   if (error === 'invalid url') {
     document.querySelector('#message').innerText = 'This website is not supported.';
   } else {
-    const todo = `👨‍💻 Fix [issue ${issueNumber}](${url}): ${issueTitle}`;
+    const todo = emoji + ` Fix [issue ${issueNumber}](${url}): ${issueTitle}`;
 
     navigator.clipboard.writeText(todo).then(function() {
       document.querySelector('#message').innerText = 'Copied!';
