70ab5cc115dbff5ec34dff2dbbe2b7cc7327b8e4
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2020-02-04 07:53:26 +0100

    make buttons work + add simple text button

--- browserAction/script.js
+++ browserAction/script.js
@@ -5,6 +5,18 @@ executing.then(() => {
   createTodo();
 }, console.error);
 
+const buttons = document.querySelectorAll('#emojis button');
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = () => {
+    createTodo(buttons[i].innerText);
+  };
+}
+
+const simpleButton = document.querySelector('#simple button');
+
+simpleButton.onclick = createSimpleText;
+
 async function createTodo(emoji = '👨‍💻') {
   const {issueTitle, issueNumber, url, error} = await browser.storage.local.get(['issueTitle', 'url', 'issueNumber', 'error']);
 
@@ -12,11 +24,24 @@ async function createTodo(emoji = '👨‍💻') {
     document.querySelector('#message').innerText = 'This website is not supported.';
   } else {
     const todo = emoji + ` Fix [issue ${issueNumber}](${url}): ${issueTitle}`;
+    document.querySelector('#emojis').classList.remove('hidden');
+    document.querySelector('#simple').classList.remove('hidden');
 
     navigator.clipboard.writeText(todo).then(function() {
-      document.querySelector('#message').innerText = 'Copied!';
+      document.querySelector('#message').innerText = `Copied with ${emoji}!`;
     }, function() {
       document.querySelector('#message').innerText = 'Unable to copy!';
     });
   }
 };
+
+async function createSimpleText() {
+  const {issueTitle, issueNumber, url, error} = await browser.storage.local.get(['issueTitle', 'url', 'issueNumber', 'error']);
+  const text = `${issueTitle} (see [issue ${issueNumber}](${url}))`;
+
+  navigator.clipboard.writeText(text).then(function() {
+    document.querySelector('#message').innerText = `Copied simple text!`;
+  }, function() {
+    document.querySelector('#message').innerText = 'Unable to copy!';
+  });
+}
